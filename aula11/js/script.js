cd4b3063bc823fc58ce4861afb974b4e594a56d3
Willian Barreto <willianblopes@gmail.com>
2023-12-19 23:51:24 -0300

    Aula + exercicio

---
+++ aula11/js/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Digite um numero');
+let num2 = prompt('Digite outro numero');
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+const resultado = num1 + num2;
+alert(`O Resultado da sua conta foi: ${resultado}`);
---
+++ aula12/index.js
@@ -0,0 +1,11 @@
+//Mudar letra conforme comentario ao lado
+
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+resB = varB;
+resC = varC;
+resA = varA;
+
+console.log(resB,resC,resA);
